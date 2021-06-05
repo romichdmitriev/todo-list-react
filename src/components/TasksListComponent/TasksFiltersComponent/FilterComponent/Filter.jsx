12129db7fc1f49c92c37a29098d5d725fd85f398
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-06-05 19:10:07 +0500

    feat: добавляю компонент Filter

---
+++ src/components/TasksListComponent/TasksFiltersComponent/FilterComponent/Filter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import getFirstUCWord from "../../../../utils/getFirstUCWord";
+
+import './filter.scss';
+
+import {FILTER_STATUS_CLASSES} from "../../../../content/FILTERS";
+
+function Filter({ activeFilter, filterName, setActiveFilter }) {
+  const setClassForFilter = () => activeFilter === filterName  ? FILTER_STATUS_CLASSES.active : FILTER_STATUS_CLASSES.inactive;
+
+  const setActiveStatusOFFilter = ({ target }) => {
+    setActiveFilter(target.dataset.filter);
+  }
+
+  return (
+    <div className={ setClassForFilter() } data-filter={filterName} onClick={ setActiveStatusOFFilter }>
+      {getFirstUCWord(filterName)}
+    </div>
+  )
+}
+
+export default Filter;
