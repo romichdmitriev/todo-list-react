424cbdf6240f218bfac1269355a569109ec51bd6
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-06-09 13:37:41 +0500

    refactor: удаляю файлы, так как данный функционал есть в подключенной библиотеке lodash

--- src/utils/getFirstUCWord.js
+++
@@ -1,3 +0,0 @@
-const getFirstUCWord = (word) => word.slice(0, 1).toUpperCase() + word.slice(1);
-
-export default getFirstUCWord;
--- src/utils/getRandomId.js
+++
@@ -1,3 +0,0 @@
-const getRandomId = () => Math.floor(Math.random() * Date.now() / 10e6);
-
-export default getRandomId;
