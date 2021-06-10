31c9af58208e783585dc2d745471259a719dd040
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-06-10 17:34:21 +0500

    fix: - переименовал tasks-list.scss в TasksList.scss - заюзал css modules в TasksList Component

--- src/components/TasksListComponent/TasksList.jsx
+++ src/components/TasksListComponent/TasksList.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import TaskItem from "./TaskItemComponent/TaskItem";
 import TasksFilters from "./TasksFiltersComponent/TasksFilters";
 
-import './tasks-list.scss';
+import styles from './TasksList.scss';
 
 const TasksList = ({ tasks, setTasks, activeFilter, setActiveFilter }) => {
   return (
-    <div className="tasks">
-      <ul className="tasks-list">
+    <div className={styles.tasks}>
+      <ul className={styles.tasksList}>
         {tasks.map((task) => (
           <TaskItem key={ task.id } currentTask={ task } tasks={ tasks } setTasks={ setTasks }/>
         ))}
