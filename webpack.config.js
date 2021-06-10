6d07559a89db1961be7a44547fa6cc0d4d721814
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-06-10 17:22:25 +0500

    fix: - в конфиге webpack применяю enviroment var - добавляю miniCSSExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,82 +3,113 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: ['@babel/polyfill', './index.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    alias: {
-      react: path.resolve(__dirname, 'node_modules', 'react'),
+const createCssFile = (isDev) => {
+  const styleLoaders = [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        modules: {
+          exportLocalsConvention: "camelCase",
+          localIdentName: `${isDev ? '[name]__[local]__[hash:base64:5]' : '[hash:base64]'}`
+        }
+      }
+    }, 'sass-loader'
+  ]
+
+  if (isDev) {
+    return styleLoaders;
+  }
+
+  // заменяем style-loader
+  styleLoaders[0] = MiniCssExtractPlugin.loader;
+  return styleLoaders;
+}
+
+module.exports = ({ development } ) => {
+  return {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill', './index.js'],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
     },
-  },
-  devtool: 'eval',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 3001
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body'
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: []
-    }),
-    new SpriteLoaderPlugin(),
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        plugins: [
-          ["jpegtran", { progressive: true }],
-        ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      alias: {
+        react: path.resolve(__dirname, 'node_modules', 'react'),
       },
-    })
-  ],
-  module:{
-    rules:  [
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader','css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
+    },
+    devtool: 'eval',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      port: 3001
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body'
+      }),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: []
+      }),
+      new SpriteLoaderPlugin(),
+      new ImageMinimizerPlugin({
+        minimizerOptions: {
+          plugins: [
+            ["jpegtran", {progressive: true}],
+          ],
         },
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader:'svg-sprite-loader',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            ...createCssFile(development)
+          ]
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'svg-sprite-loader',
+              options: {
+                extract: true,
+                publicPath: './assets/icons/',
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              extract: true,
-              publicPath: './assets/icons/',
+              plugins: ['lodash'],
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
             }
           }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['lodash'],
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react'
-            ]
-          }
-        }
-      },
-    ]
+        },
+      ]
+    }
   }
-};
+}
