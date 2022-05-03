093132c1bd0108759bc5a1845afc3bb2f77b13ee
Dmitriev R.R <education.romichdmitriev@gmail.com>
2022-05-03 19:24:00 +0500

    feat: add text constants

---
+++ src/constants/text.js
@@ -0,0 +1,13 @@
+const CONST_TEXT = {
+  // header
+  todo: 'TODO',
+
+  // Task Input Create
+  todoCreateInputPlaceholder: 'Create a new todo...',
+
+  // Tasks Filters
+  todoCountRemain: 'items left',
+  deleteCompletedTodos: 'Clear Completed',
+};
+
+export default CONST_TEXT;
---
+++ src/constants/unique-keys.js
@@ -0,0 +1,17 @@
+const UNIQUE_TEXT_KEYS = {
+  todos: 'todos',
+
+  // theme
+  theme: 'theme',
+  dark: 'dark',
+  light: 'light',
+  dataThemeAttr: 'data-theme',
+
+  // filters
+  filter: 'filter',
+  all: 'all',
+  completed: 'completed',
+  active: 'active',
+};
+
+export default UNIQUE_TEXT_KEYS;
