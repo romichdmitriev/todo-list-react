ce41e2fbbc2f3a66b70191199c6471ea2c0107b9
Dmitriev R.R <education.romichdmitriev@gmail.com>
2022-05-03 19:26:01 +0500

    feat: add common hooks

--- src/hooks/useDispatchActions.js
+++ src/hooks/useDispatchActions.js
@@ -1,7 +1,24 @@
-import { useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import useDispatch from '@hooks/useDispatch';
+import { useMemo } from 'react';
 
-export const useDispatchActions = (actions) => {
+/**
+    @param actions объект из action creators (payload: any) => { type: string, payload }
+*/
+
+const useDispatchActions = (actions) => {
   const dispatch = useDispatch();
-  return bindActionCreators(actions, dispatch);
+
+  return useMemo(() => {
+    const dispatchdedActions = {};
+
+    for (let [actionName, action] of Object.entries(actions)) {
+      dispatchdedActions[actionName] = (payload) => {
+        dispatch(action(payload));
+      };
+    }
+
+    return dispatchdedActions;
+  }, []);
 };
+
+export default useDispatchActions;
---
+++ src/hooks/useEffectOnce.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+const useEffectOnce = (effect) => {
+  useEffect(() => {
+    effect();
+  }, []);
+};
+
+export default useEffectOnce;
